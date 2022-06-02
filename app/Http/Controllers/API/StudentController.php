3adb29b6104a70e476930a1fca5a0c066da1669e
kaderabdul <kaderabdul7980@gmail.com>
2022-06-03 01:41:52 +0600

    store data in database

--- app/Http/Controllers/API/StudentController.php
+++ app/Http/Controllers/API/StudentController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+            $student = new Student;
+            $student->name = $request->input('name');
+            $student->course = $request->input('course');
+            $student->email = $request->input('email');
+            $student->phone = $request->input('phone');
+            $student->save();
+
+            return response()->json([
+                'status'=> 200,
+                'message'=>'Student Added Successfully',
+            ]);
+
+    }
 }
