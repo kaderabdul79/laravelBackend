54724d7c9f4775f8238ea961f1e0c9f53eeaf683
kaderabdul <kaderabdul7980@gmail.com>
2022-06-05 06:51:25 +0600

    created function for edit and update ,for update data also addedd validation

--- app/Http/Controllers/API/StudentController.php
+++ app/Http/Controllers/API/StudentController.php
@@ -51,6 +51,69 @@ class StudentController extends Controller
         ]);
     }
 
+    public function edit($id)
+    {
+        $student = Student::find($id);
+        if($student)
+        {
+            return response()->json([
+                'status'=> 200,
+                'student' => $student,
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'status'=> 404,
+                'message' => 'No Student ID Found',
+            ]);
+        }
+
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|max:191',
+            'course'=>'required|max:191',
+            'email'=>'required|email|max:191',
+            'phone'=>'required|max:10|min:10',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'status'=> 422,
+                'validationErrors'=> $validator->errors(),
+            ]);
+        }
+        else
+        {
+            $student = Student::find($id);
+            if($student)
+            {
+
+                $student->name = $request->input('name');
+                $student->course = $request->input('course');
+                $student->email = $request->input('email');
+                $student->phone = $request->input('phone');
+                $student->update();
+
+                return response()->json([
+                    'status'=> 200,
+                    'message'=>'Student Updated Successfully',
+                ]);
+            }
+            else
+            {
+                return response()->json([
+                    'status'=> 404,
+                    'message' => 'No Student ID Found',
+                ]);
+            }
+        }
+    }
+
 
     public function destroy($id)
     {
